87053dca1498e565660a482d46722d54305b96d9
RealLionelMuhire <muhirelionel@gmail.com>
2023-07-02 16:18:48 +0200

    swapping c to py

--- c_fact.c
+++ c_fact.c
@@ -2,74 +2,71 @@
 #include <stdlib.h>
 #include <gmp.h>
 
-void factor(mpz_t num) {
-    mpz_t two;
-    mpz_t sqn;
-    mpz_init(two);
-    mpz_init(sqn);
+void factor(mpz_t n) {
+    mpz_t factor_num;
+    mpz_init(factor_num);
 
-    mpz_set_ui(two, 2);
-    mpz_sqrt(sqn, num);
+    gmp_printf("%Zd=", n);
 
-    if (mpz_even_p(num)) {
-        mpz_divexact_ui(num, num, 2);
-        gmp_printf("%Zd*%Zd\n", num, two);
-    } else {
-        if (mpz_even_p(sqn)) {
-            mpz_add_ui(sqn, sqn, 1);
-        }
+    mpz_t x, y, d;
+    mpz_init(x);
+    mpz_init(y);
+    mpz_init(d);
 
-        mpz_t i;
-        mpz_init_set_ui(i, 3);
-        mpz_t rem;
-        mpz_init(rem);
+    mpz_set_ui(x, 2);
+    mpz_set_ui(y, 2);
 
-        while (mpz_cmp(i, sqn) <= 0) {
-            mpz_tdiv_r(rem, num, i);
-            if (mpz_cmp_ui(rem, 0) == 0) {
-                mpz_tdiv_q(num, num, i);
-                gmp_printf("%Zd*%Zd\n", i, num);
-                break;
-            }
-            mpz_add_ui(i, i, 2);
-        }
+    while (mpz_cmp_ui(d, 1) == 0) {
+        mpz_mul(x, x, x);
+        mpz_add_ui(x, x, 1);
+        mpz_mod(x, x, n);
 
-        if (mpz_cmp(i, sqn) > 0) {
-            gmp_printf("%Zd=%Zd*1\n", num, num);
-        }
+        mpz_mul(y, y, y);
+        mpz_add_ui(y, y, 1);
+        mpz_mod(y, y, n);
 
-        mpz_clear(i);
-        mpz_clear(rem);
+        mpz_sub(d, x, y);
+        mpz_abs(d, d);
+        mpz_gcd(d, d, n);
     }
 
-    mpz_clear(two);
-    mpz_clear(sqn);
+    if (mpz_cmp(d, n) == 0) {
+        gmp_printf("%Zd is prime\n", n);
+    } else {
+        gmp_printf("%Zd*", d);
+        mpz_divexact(n, n, d);
+        factor(n);
+    }
+
+    mpz_clear(factor_num);
+    mpz_clear(x);
+    mpz_clear(y);
+    mpz_clear(d);
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Please provide a file path as an argument!\n");
-        return 1;
+        return 0;
     }
 
     FILE *file = fopen(argv[1], "r");
     if (file == NULL) {
         printf("File '%s' not found.\n", argv[1]);
-        return 1;
+        return 0;
     }
 
-    char line[100];
-    mpz_t num;
-    mpz_init(num);
-
+    char line[1024];
     while (fgets(line, sizeof(line), file)) {
+        mpz_t num;
+        mpz_init(num);
         mpz_set_str(num, line, 10);
-        gmp_printf("%Zd=", num);
         factor(num);
+        mpz_clear(num);
     }
 
-    mpz_clear(num);
     fclose(file);
+
     return 0;
 }
 
---
+++ c_rsa.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+#include <math.h>
+
+void rsa(const char* file_path) {
+    FILE* file = fopen(file_path, "r");
+    if (file == NULL) {
+        printf("File '%s' not found.\n", file_path);
+        return;
+    }
+
+    char line[1024];
+    while (fgets(line, sizeof(line), file)) {
+        mpz_t num;
+        mpz_init(num);
+        mpz_set_str(num, line, 10);
+
+        gmp_printf("%Zd=", num);
+
+        if (mpz_cmp_ui(num, 0) < 0 || mpz_cmp_ui(num, 2) == 0) {
+            gmp_printf("%Zd*1\n", num);
+        } else {
+            mpz_t i;
+            mpz_init(i);
+            mpz_set_ui(i, 3);
+            mpz_t sqrt_num;
+            mpz_init(sqrt_num);
+            mpz_sqrt(sqrt_num, num);
+
+            while (mpz_cmp(i, sqrt_num) <= 0) {
+                if (mpz_divisible_p(num, i)) {
+                    mpz_divexact(num, num, i);
+                    gmp_printf("%Zd*%Zd\n", num, i);
+                    break;
+                }
+                mpz_add_ui(i, i, 2);
+            }
+
+            if (mpz_cmp(i, sqrt_num) > 0) {
+                gmp_printf("%Zd*1\n", num);
+            }
+
+            mpz_clear(sqrt_num);
+            mpz_clear(i);
+        }
+
+        mpz_clear(num);
+    }
+
+    fclose(file);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Please provide a file path as argument!\n");
+        return 0;
+    }
+
+    rsa(argv[1]);
+
+    return 0;
+}
+
