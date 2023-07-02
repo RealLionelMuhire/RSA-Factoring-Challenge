eaaa169c696badc6dc11b93adc54ec38eaa6fb1d
RealLionelMuhire <muhirelionel@gmail.com>
2023-07-02 16:34:59 +0200

    fixin multiple speeds

--- c_fact.c
+++ c_fact.c
@@ -1,59 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
+#include <math.h>
 
-void factor(mpz_t n) {
-    mpz_t factor_num;
-    mpz_init(factor_num);
-
-    gmp_printf("%Zd=", n);
-
-    mpz_t x, y, d;
-    mpz_init(x);
-    mpz_init(y);
-    mpz_init(d);
-
-    mpz_set_ui(x, 2);
-    mpz_set_ui(y, 2);
-
-    while (mpz_cmp_ui(d, 1) == 0) {
-        mpz_mul(x, x, x);
-        mpz_add_ui(x, x, 1);
-        mpz_mod(x, x, n);
-
-        mpz_mul(y, y, y);
-        mpz_add_ui(y, y, 1);
-        mpz_mod(y, y, n);
-
-        mpz_sub(d, x, y);
-        mpz_abs(d, d);
-        mpz_gcd(d, d, n);
-    }
-
-    if (mpz_cmp(d, n) == 0) {
-        gmp_printf("%Zd is prime\n", n);
-    } else {
-        gmp_printf("%Zd*", d);
-        mpz_divexact(n, n, d);
-        factor(n);
-    }
-
-    mpz_clear(factor_num);
-    mpz_clear(x);
-    mpz_clear(y);
-    mpz_clear(d);
-}
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Please provide a file path as an argument!\n");
-        return 0;
-    }
-
-    FILE *file = fopen(argv[1], "r");
+void rsa(const char* file_path) {
+    FILE* file = fopen(file_path, "r");
     if (file == NULL) {
-        printf("File '%s' not found.\n", argv[1]);
-        return 0;
+        printf("File '%s' not found.\n", file_path);
+        return;
     }
 
     char line[1024];
@@ -61,11 +15,63 @@ int main(int argc, char *argv[]) {
         mpz_t num;
         mpz_init(num);
         mpz_set_str(num, line, 10);
-        factor(num);
+
+        gmp_printf("%Zd=", num);
+
+        while (mpz_even_p(num)) {
+            printf("2*");
+            mpz_divexact_ui(num, num, 2);
+        }
+
+        if (mpz_cmp_ui(num, 1) <= 0) {
+            printf("1\n");
+        } else {
+            mpz_t i, x, y, factor;
+            mpz_init(i);
+            mpz_init(x);
+            mpz_init(y);
+            mpz_init(factor);
+            mpz_set_ui(i, 1);
+            mpz_set_ui(x, 2);
+            mpz_set_ui(y, 2);
+            mpz_set_ui(factor, 1);
+
+            while (mpz_cmp_ui(factor, 1) == 0) {
+                mpz_add_ui(i, i, 1);
+                mpz_mul(x, x, x);
+                mpz_add_ui(x, x, 1);
+                mpz_mod(x, x, num);
+
+                mpz_mul(y, y, y);
+                mpz_add_ui(y, y, 1);
+                mpz_mod(y, y, num);
+
+                mpz_sub(factor, x, y);
+                mpz_abs(factor, factor);
+                mpz_gcd(factor, factor, num);
+            }
+
+            gmp_printf("%Zd\n", factor);
+
+            mpz_clear(i);
+            mpz_clear(x);
+            mpz_clear(y);
+            mpz_clear(factor);
+        }
+
         mpz_clear(num);
     }
 
     fclose(file);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Please provide a file path as argument!\n");
+        return 0;
+    }
+
+    rsa(argv[1]);
 
     return 0;
 }
